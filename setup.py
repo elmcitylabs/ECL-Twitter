7bff82291a34d29509e52775ae6aa95633d0beaa
Dan Loewenherz <dloewenherz@gmail.com>
2011-07-08 15:44:48 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python
+
+from setuptools import setup
+
+setup(
+    name = 'ecl_twitter',
+    version = '1.0',
+    url = 'http://elmcitylabs.com',
+    license = 'BSD',
+    description = 'Easy Twitter integration for Django.',
+    author = 'Dan Loewenherz',
+    author_email = 'dan@elmcitylabs.com',
+    packages=['ecl_twitter'],
+)
+
