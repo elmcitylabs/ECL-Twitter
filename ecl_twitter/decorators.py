8e30bd7e48cdd0f4246940097cecd5b3dd1d7290
James Johnson <jamesjohnson11@gmail.com>
2012-03-09 18:03:23 -0500

    bump to version 0.3.4.

--- ecl_twitter/decorators.py
+++ ecl_twitter/decorators.py
@@ -1,8 +1,26 @@
-import logging
+from functools import wraps
+
 import twitter
+import logging
+from django.http import HttpResponseRedirect
 from signals import post_twitter_auth
 
 logger = logging.getLogger(__name__)
+
+def twitter_begin(fun):
+    @wraps(fun)
+    def k(request, *args, **kwargs):
+        client = twitter.Twitter()
+        data = client.generate_authorization_url()
+        token = data['oauth_token']
+        secret = data['oauth_token_secret']
+
+        request.session['temporary_oauth_token'] = token
+        request.session['temporary_oauth_secret'] = secret
+        url = twitter.TWITTER_BASE_URL + twitter.TWITTER_OAUTH_AUTHORIZE
+        return HttpResponseRedirect(url + '?oauth_token=' + token)
+
+    return k
 
 def twitter_callback(fun):
     def k(request, *args, **kwargs):
