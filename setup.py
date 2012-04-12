0fb4098e3f646e6d586be183bb5cb2ae348daed6
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-12 10:41:33 -0700

    update package classifiers

--- setup.py
+++ setup.py
@@ -16,7 +16,20 @@ setup(
     author = ecl_twitter.__author__,
     author_email = ecl_twitter.__email__,
     packages=['ecl_twitter'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        ],
     install_requires=['requests', 'objectifier>=1.1.2'],
     package_data={'': ['LICENSE']},
-)
+    )
 
