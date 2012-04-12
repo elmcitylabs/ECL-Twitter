4792cca436ec0b7bc20d21bd52b4af12a18e6db0
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-12 10:15:02 -0700

    fix import error

--- ecl_twitter/__init__.py
+++ ecl_twitter/__init__.py
@@ -9,7 +9,11 @@ ecl_twitter
 
 """
 
-from .twitter import Twitter
+try:
+    from .twitter import Twitter
+except ImportError:
+    # One of the required libraries hasn't been installed yet.
+    pass
 
 __all__ = ['Twitter']
 __version__ = "1.0.0"
@@ -18,5 +22,4 @@ __copyright__ = "Copyright 2012, Elm City Labs, LLC"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@elmcitylabs.com"
 __license__ = "Apache 2.0"
-
 
